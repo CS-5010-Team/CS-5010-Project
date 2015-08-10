3bf584568785952358ebbff332ecec05c47caa5a
Fanjia Kong <fk9mp@virginia.edu>
2015-08-09 22:20:05 -0400

    Create a line chart by the time

--- project_Ken.py
+++ project_Ken.py
@@ -1,24 +1,65 @@
-from numpy import *
+import numpy as np
 import pandas as pd
-import datetime
+import matplotlib.pyplot as plt
 
-#read in the file
-df = pd.read_csv("gridwatch-small.csv", header = 0)
 
-#strip the space in names of column
-#aka: clean the data
-df.columns = df.columns.str.strip()
+def line_plot(s):
+	var_names = input("The trend for what variable you want to see?(seperate by ',') ")
+	var_name = [x.strip() for x in var_names.split(',')]
+
+	s.plot("timestamp", var_name, label=var_name)
+	plt.legend()
+	plt.show()
+
+	menu(s)
+	return
+
+def menu(s):
+	print("\nThis is the menu for analysis: ")
+	print("--- 1 --- Line chart with timestamp ")
+
+	option = input("\nType in the number or -1 to quit: ")
+
+	if option == "-1" : 
+		return
+	elif option == "1":
+		line_plot(s)
+	else:
+		return
+
+	return
+
+
+def beginning():
+	#read in the file
+	df = pd.read_csv("gridwatch-small.csv", header = 0)
+
+	#strip the space in names of column
+	#aka: clean the data
+	df.columns = df.columns.str.strip()
  
-#input a date and return all the rows for that day
-time_lookingfors = input("Input the time in the format YYYY-MM-DD(seperate by ','):")
-time_lookingfor = [y.strip() for y in time_lookingfors.split(',')]
-s = df[df['timestamp'].str.contains('|'.join(time_lookingfor))]
+	#input a date and return all the rows for that day
+	time_lookingfors = input("Input the time in the format YYYY-MM-DD(seperate by ','):")
+	time_lookingfor = [y.strip() for y in time_lookingfors.split(',')]
 
-#input the columns you need
-titles = input("Input the title you are looking for(seperate by ',') or type 'all': ")
-if (titles == 'all'):
-	s = s[s.columns.values]
-else:
-	title_lookingfor = [x.strip() for x in titles.split(',')]
-	s = s[title_lookingfor]
-print(s)
+	s = df[df['timestamp'].str.contains('|'.join(time_lookingfor))]
+
+	#input the columns you need
+	titles = input("Input the title you are looking for(seperate by ',') or type 'all': ")
+	if (titles == 'all'):
+		s = s[s.columns.values]
+	else:
+		title_lookingfor = [x.strip() for x in titles.split(',')]
+		s = s[title_lookingfor]
+	#print(s)
+
+	#save the dataframe to a new .csv file
+	s.to_csv("extract.csv", sep = ',')
+
+	menu(s);
+
+	return
+
+#The entire program starts here.
+beginning()
+
