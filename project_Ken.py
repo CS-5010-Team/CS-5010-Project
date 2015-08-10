648ced9d5475ede73dd2d9b20ad26e69ade6421e
Fanjia Kong <fk9mp@virginia.edu>
2015-08-10 15:51:42 -0400

    Update with function of pie chart and improve the user interface.

--- project_Ken.py
+++ project_Ken.py
@@ -14,9 +14,20 @@ def line_plot(s):
 	menu(s)
 	return
 
+def pie_chart(s):
+	ans = s.sum(axis=0)
+	plt.pie(ans.get_values(), labels=ans.keys(), autopct='%1.1f%%')
+	plt.axis('equal')
+	plt.show()
+	
+	menu(s)
+	return
+
+
 def menu(s):
 	print("\nThis is the menu for analysis: ")
-	print("--- 1 --- Line chart with timestamp ")
+	print("     1 --- Line chart with timestamp ")
+	print("     2 --- Pie chart the consumption in that date ")
 
 	option = input("\nType in the number or -1 to quit: ")
 
@@ -24,6 +35,8 @@ def menu(s):
 		return
 	elif option == "1":
 		line_plot(s)
+	elif option == "2":
+		pie_chart(s)
 	else:
 		return
 
@@ -32,13 +45,19 @@ def menu(s):
 
 def beginning():
 	#read in the file
+	print("\nWELCOME TO THE UK ENERGY COMSUMPTION!")
+	print("\n********************************************************")
+	print("\nINITIALING............")
 	df = pd.read_csv("gridwatch-small.csv", header = 0)
 
 	#strip the space in names of column
 	#aka: clean the data
 	df.columns = df.columns.str.strip()
+	print("\nDONE!\n")
  
 	#input a date and return all the rows for that day
+	print("********************************************************")
+	print("\nEXTRACT DATA FROM DATASET............ \n")
 	time_lookingfors = input("Input the time in the format YYYY-MM-DD(seperate by ','):")
 	time_lookingfor = [y.strip() for y in time_lookingfors.split(',')]
 
@@ -51,11 +70,13 @@ def beginning():
 	else:
 		title_lookingfor = [x.strip() for x in titles.split(',')]
 		s = s[title_lookingfor]
-	#print(s)
+	
 
 	#save the dataframe to a new .csv file
+	#print(s)
 	s.to_csv("extract.csv", sep = ',')
-
+	print("\nEXTRACT SUCCEFULLY!!\n")
+	print("********************************************************")
 	menu(s);
 
 	return
