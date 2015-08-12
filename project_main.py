8631bbc7533a984de7b9a44266ce806397b32ad8
Mason Montgomery <mtm3ad@virginia.edu>
2015-08-12 17:51:32 -0400

    basic linear model

--- project_main.py
+++ project_main.py
@@ -14,7 +14,7 @@ def main():
     print("\n********************************************************")
     print("\n**    WELCOME TO THE ENERGY GRID DATA MANIPULATOR!    **")
     print("\n********************************************************")
-    print("\nINITIALING............")
+    print("\nINITIALIZING............")
     #Get a valid source filename
     input_file = get_filename()
 
@@ -221,7 +221,8 @@ def get_frequency():
 def plot_menu(frame):
     print("\nWhat kind of plot would you like to generate?")
     print("--- 1 --- Bar Chart\n--- 2 --- Time series line graph \n" +
-          "--- 3 --- Covariance matrix\n--- 4 --- Autocorrelation plot")
+          "--- 3 --- Covariance matrix\n--- 4 --- Autocorrelation plot\n" +
+          "--- 5 --- Linear Model")
     
     while True:
         option = input("\nChoose an option:")
@@ -241,6 +242,8 @@ def plot_menu(frame):
         elif option == "4":
             auto_correlation(frame)
             return
+#        elif option == "5":
+#            linearmodel(frame)
         else:
            print("Invalid input.  Try again.")
 
